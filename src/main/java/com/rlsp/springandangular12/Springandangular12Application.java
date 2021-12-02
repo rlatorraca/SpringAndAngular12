8b34fbbb6b381975ec1ce5dee3dd224e05047631
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-12-02 15:01:57 -0400

    Raw App

---
+++ src/main/java/com/rlsp/springandangular12/Springandangular12Application.java
@@ -0,0 +1,13 @@
+package com.rlsp.springandangular12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springandangular12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springandangular12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rlsp/springandangular12/Springandangular12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlsp.springandangular12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springandangular12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
